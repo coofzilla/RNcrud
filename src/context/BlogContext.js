2534e24afa2a063e2bce34a8f2e0d4dd6c1ef3da
Jay Hernandez <jeric614@yahoo.com>
2022-01-20 09:42:12 +0900

    added blogs w/context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
 //named export import with { BlogProvider }
 export const BlogProvider = ({ children }) => {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  const addBlogPost = () => {
+    setBlogPosts([
+      ...blogPosts,
+      { title: `Blog Post #${blogPosts.length + 1}` },
+    ]);
+  };
+
   return (
-    <BlogContext.Provider value={[1, 2, 3]}>{children}</BlogContext.Provider>
+    //All consumers that are descendants of a Provider will re-render whenever the Provider’s value prop changes
+    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+      {children}
+    </BlogContext.Provider>
   );
 };
 
 export default BlogContext;
+
+//vanilla explanation for above
+// let blogs = [];
+
+// const addBlogs = () => {
+//   blogs = [...blogs, { title: 'blog test 1' }];
+//   blogs = [...blogs, { title: 'blog test 2' }];
+//   blogs = [...blogs, { title: 'blog test 3' }];
+
+// };
+// addBlogs();
+// console.log(blogs);
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,24 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext);
+  //A component calling useContext will always re-render when the context value changes.
+  //If re-rendering the component is expensive, you can optimize it by using memoization.
+  const { data, addBlogPost } = useContext(BlogContext);
 
   return (
     <View>
       <Text>Index Screen</Text>
-      <Text>{value}</Text>
+      {/* same as this onPress={() => addBlogPost()}; makes ref to function to be called */}
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
