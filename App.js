b3362e49bf2233210cf7f39b73ce1ed1d805a649
coofzilla <jeric614@yahoo.com>
2022-01-24 08:37:36 +0900

    creation screen added

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import IndexScreen from "./src/screens/IndexScreen";
 import { Provider as BlogProvider } from "./src/context/BlogContext";
+import IndexScreen from "./src/screens/IndexScreen";
 import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
 
 const App = () => {
   const { Navigator, Screen } = createNativeStackNavigator();
@@ -18,6 +19,7 @@ const App = () => {
       >
         <Screen name="Index" component={IndexScreen} />
         <Screen name="Show" component={ShowScreen} />
+        <Screen name="Create" component={CreateScreen} />
       </Navigator>
     </NavigationContainer>
   );
