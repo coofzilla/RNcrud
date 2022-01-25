fc17c4c7b22745faf1a4750cbe2cff0895f2b49a
coofzilla <jeric614@yahoo.com>
2022-01-25 10:17:36 +0900

    extracted blog form

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,22 +1,13 @@
-import React, { useState, useContext } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
 import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const EditScreen = ({ route: { params } }) => {
   const { state } = useContext(Context);
   const blogPost = state.find((post) => post.id === params.id);
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
 
-  return (
-    <View>
-      <Text>Edit Title:</Text>
-      <TextInput
-        value={title}
-        onChangeText={(newTitle) => setTitle(newTitle)}
-      />
-    </View>
-  );
+  return <BlogPostForm />;
 };
 
 const styles = StyleSheet.create({});
