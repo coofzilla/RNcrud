acbcd798f0513aa93990f15538bd2ddfcd43a261
coofzilla <jeric614@yahoo.com>
2022-01-25 10:01:25 +0900

    initialized editscreen state from context

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,10 +1,20 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useContext } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Context } from "../context/BlogContext";
 
 const EditScreen = ({ route: { params } }) => {
+  const { state } = useContext(Context);
+  const blogPost = state.find((post) => post.id === params.id);
+  const [title, setTitle] = useState(blogPost.title);
+  const [content, setContent] = useState(blogPost.content);
+
   return (
     <View>
-      <Text>Edit Screen - {params.id}</Text>
+      <Text>Edit Title:</Text>
+      <TextInput
+        value={title}
+        onChangeText={(newTitle) => setTitle(newTitle)}
+      />
     </View>
   );
 };
